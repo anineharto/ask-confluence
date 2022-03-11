024e4370c88975df39be7bb3b001a111c625cc90
Anine Harto <anine.harto@accenture.com>
2022-03-11 13:04:27 +0100

    Refactored from Flask to FastAPI

---
+++ ask_confluence/app/resource/main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from ask_confluence.app.service.answer_service import get_answer_from_confluence
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/healthCheck")
+async def health_check():
+    return {"status": "Healthy"}
+
+@app.post("/askConfluence")
+async def ask_question(question: str):
+    try:
+        return {"answer": get_answer_from_confluence(question)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Asking failed. {e}")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127:0:0:1", port=8000)
